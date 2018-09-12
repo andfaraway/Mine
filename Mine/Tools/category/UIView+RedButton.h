15670b7160b9d6af68a204c2eee0d9697f5db71e
libin <bin@lideMacBook-Pro.local>
2018-09-12 10:38:30 +0800

    add WMDragView

---
+++ Mine/Tools/category/UIView+RedButton.h
@@ -0,0 +1,13 @@
+//
+//  UIView+RedButton.h
+//  Mine
+//
+//  Created by li on 2018/4/26.
+//  Copyright © 2018年 Cnmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (RedButton)
+
+@end
---
+++ Mine/Tools/category/UIView+RedButton.m
@@ -0,0 +1,13 @@
+//
+//  UIView+RedButton.m
+//  Mine
+//
+//  Created by li on 2018/4/26.
+//  Copyright © 2018年 Cnmobi. All rights reserved.
+//
+
+#import "UIView+RedButton.h"
+
+@implementation UIView (RedButton)
+
+@end
